8d64f11b341372019f7429ea70c8960e16fa84d0
9jh1 <tkf.x1os@gmail.com>
2025-09-22 12:49:39 +1200

    Mon 22 Sep 2025 12:49:39 NZST

--- ml.c
+++ ml.c
@@ -1,6 +1,25 @@
 #include <stdio.h> 
+#include "lib/plib.h"
 
-int main(){
-	printf("Hello World\n");
+void help() {
+	printf("motionlayer [options]\noptions:\n");
+	pl_help();
+}
+
+int main(const int argc, const char *argv[]){
+	pl_arg *help_a = PL_A(
+			.name = "--help",
+			.description = "Show this dialog"
+			); 
+	
+	// init plib 
+	const pl_return_type ret = PL_PROC();
+	if(ret != PL_SUCCESS){
+		help();
+		printf("%s -> %s\n",pl_return_type_string[ret],PL_LAST_ARG);
+	} else {
+		// continue
+		printf("passed\n");
+	}
 	return 0;
 }

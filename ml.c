9cb9b43db3a889a29a9682ec845f0fee0fda1caf
9jh1 <tkf.x1os@gmail.com>
2025-09-22 13:47:01 +1200

    Mon 22 Sep 2025 13:47:01 NZST

--- ml.c
+++ ml.c
@@ -1,4 +1,5 @@
 #include <stdio.h> 
+#include <stdlib.h>
 #include "lib/plib.h"
 
 void help() {
@@ -6,20 +7,60 @@ void help() {
 	pl_help();
 }
 
+void _exit(int code){
+	pl_exit();
+	printf("\rmotionlayer exited with code %d\n", code);
+	exit(code);
+}
+
 int main(const int argc, const char *argv[]){
+	// SET ARGUMENTS ==========================
 	pl_arg *help_a = PL_A(
 			.name = "--help",
 			.description = "Show this dialog"
 			); 
+
+	pl_arg *xine_a = PL_A(
+			.name = "--dual",
+			.description = "fills wallpaper over two monitors",
+			);
+
+	pl_arg *geom_a = PL_A(
+			.name = "--geometry",
+			.description = "set custom mpv geometry eg 1920x1070+0+0",
+			.takes_value = TAKES_VALUE,
+			);
+
+	pl_arg *zoom_a = PL_A(
+			.name = "--zoom",
+			.description = "set video zoom, default 0.0",
+			.takes_value=TAKES_VALUE,
+			);
+
+	pl_arg *volm_a = PL_A(
+			.name = "--volume",
+			.description = "set video volume, default 0",
+			.takes_value = TAKES_VALUE,
+			);
 	
-	// init plib 
+	// INIT PLIB ==============================
 	const pl_return_type ret = PL_PROC();
 	if(ret != PL_SUCCESS){
 		help();
-		printf("%s -> %s\n",pl_return_type_string[ret],PL_LAST_ARG);
+		
+		// HANDLE ERROR =========================
+		printf("%s caused by '%s'\n",
+				pl_return_type_string[ret], 
+				PL_LAST_ARG);
+
+		exit(1);
 	} else {
-		// continue
-		printf("passed\n");
+		// RUN HELP DIALOG ======================
+		if(pl_arg_run(help_a) == PL_SUCCESS) 
+			help();
+
+
 	}
-	return 0;
+
+	exit(0);
 }
